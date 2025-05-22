0a6856ee1116e62483aeedb9be479d9e4eaacf44
IcaroRaphael <icaro2551@gmail.com>
2025-05-22 19:02:49 -0300

    Ex1008

--- Beecrowd/ConsoleApp1/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿string nome = Console.ReadLine();
-int idade = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"seu nome é {nome} e você tem {idade} anos");
---
+++ Beecrowd/Ex1008/Program.cs
@@ -0,0 +1,8 @@
+﻿int numeroFuncionario = int.Parse(Console.ReadLine());
+int horasTrabalhadas = int.Parse(Console.ReadLine());
+double valorPorHora = double.Parse(Console.ReadLine());
+
+double salario = horasTrabalhadas * valorPorHora;
+
+System.Console.WriteLine($"NUMBER = {numeroFuncionario}");
+System.Console.WriteLine($"SALARY = U$ {salario:F2}");
