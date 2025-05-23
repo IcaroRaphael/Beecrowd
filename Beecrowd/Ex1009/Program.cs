408504e826da1d912092c82314623683498c0fc4
IcaroRaphael <icaro2551@gmail.com>
2025-05-22 21:35:32 -0300

    Ex1009

---
+++ Beecrowd/Ex1009/Program.cs
@@ -0,0 +1,7 @@
+﻿string nomeVendedor = Console.ReadLine();
+double salarioFixoVendedor = double.Parse(Console.ReadLine());
+double vendasVendedor = double.Parse(Console.ReadLine());
+
+double salarioFinalVendedor = (0.15 * vendasVendedor) + salarioFixoVendedor;
+
+System.Console.WriteLine($"TOTAL = R$ {salarioFinalVendedor:F2}");
