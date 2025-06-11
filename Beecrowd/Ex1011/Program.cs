ba22b03655a07baa6546d51b378acd2212364044
IcaroRaphael <icaro2551@gmail.com>
2025-06-11 12:42:21 -0300

    Ex1012

---
+++ Beecrowd/Ex1011/Program.cs
@@ -0,0 +1,13 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args)
+    {
+        double raio = double.Parse(Console.ReadLine());
+        const double PI = 3.14159;
+        double volume = (4.0 / 3) * PI * Math.Pow(raio, 3);
+        Console.WriteLine($"VOLUME = {volume:f3}");
+    }
+
+}
---
+++ Beecrowd/Ex1012/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        const double PI = 3.14159;
+
+        string[] entrada = Console.ReadLine().Split(' ');
+        double a = double.Parse(entrada[0]);
+        double b = double.Parse(entrada[1]);
+        double c = double.Parse(entrada[2]);
+
+        double areaDoTriangulo = a * c / 2.0;
+        double areaDoCirculo = PI * Math.Pow(c, 2);
+        double areaDoTrapezio = (a + b) * c / 2.0;
+        double areaDoQuadrado = b * b;
+        double areaDoRetangulo = a * b;
+
+        Console.WriteLine($"TRIANGULO: {areaDoTriangulo:f3}");
+        Console.WriteLine($"CIRCULO: {areaDoCirculo:f3}");
+        Console.WriteLine($"TRAPEZIO: {areaDoTrapezio:f3}");
+        Console.WriteLine($"QUADRADO: {areaDoQuadrado:f3}");
+        Console.WriteLine($"RETANGULO: {areaDoRetangulo:f3}");
+
+    }
+
+}
