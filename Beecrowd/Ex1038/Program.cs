d5171168dbe7d10fc1f6bbd1c76d911535a21598
IcaroRaphael <icaro2551@gmail.com>
2025-06-18 09:44:18 -0300

    Ex1038

---
+++ Beecrowd/Ex1038/Program.cs
@@ -0,0 +1,8 @@
+﻿double[] precos = new double[5] { 4.00, 4.50, 5.00, 2.00, 1.50 };
+
+string[] entrada = Console.ReadLine().Split(' ');
+int item = int.Parse(entrada[0]);
+int quantidade = int.Parse(entrada[1]);
+
+double precoTotal = quantidade * (precos[item - 1]);
+Console.WriteLine($"Total: R$ {precoTotal:f2}");
