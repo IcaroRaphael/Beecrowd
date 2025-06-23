aed1343b53de890bee4ebd76039207507ad851cb
IcaroRaphael <icaro2551@gmail.com>
2025-06-23 11:37:23 -0300

    Ex1041

--- Beecrowd/Ex1040/Program.cs
+++ Beecrowd/Ex1040/Program.cs
@@ -7,17 +7,21 @@ double num4 = double.Parse(valores[3]);
 
 double media = ((num1 * 2) + (num2 * 3) + (num3 * 4) + (num4 * 1)) / (2 + 3 + 4 + 1);
 
-Console.WriteLine($"Media: {media:f1}");
+Console.WriteLine($"Media: {Math.Round(media,1):0.0}");
 
 if (media >= 7.0)
 {
     Console.WriteLine("Aluno aprovado.");
 }
+else if (media < 5)
+{
+    Console.WriteLine("Aluno reprovado.");
+}
 else if (media < 7 && media >= 5)
 {
     Console.WriteLine("Aluno em exame.");
-    Console.Write("Nota do exame: ");
     double recuperacao = double.Parse(Console.ReadLine());
+    Console.WriteLine($"Nota do exame: {Math.Round(recuperacao, 1):0.0}");
     double mediaFinal = (recuperacao + media) / 2;
     if (mediaFinal >= 5)
     {
@@ -27,9 +31,5 @@ else if (media < 7 && media >= 5)
     {
         Console.WriteLine("Aluno reprovado.");
     }
-    Console.WriteLine($"Media final: {mediaFinal:f1}");
-}
-else if (media < 5)
-{
-    Console.WriteLine("Aluno reprovado.");
+    Console.WriteLine($"Media final: {Math.Round(mediaFinal, 1):0.0}");
 }
