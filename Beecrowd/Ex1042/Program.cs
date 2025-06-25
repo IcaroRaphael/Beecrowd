e63a8fea426e67a6a70d7e7a551e6fdc10f87a3c
IcaroRaphael <icaro2551@gmail.com>
2025-06-25 10:51:47 -0300

    Ex1042

---
+++ Beecrowd/Ex1042/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+string[] entrada = Console.ReadLine().Split(' ');
+
+List<int> valoresOrdemCrescente = new List<int>();
+List<int> temp = new List<int>();
+
+//Adicionando todos os valores em uma lista temporária
+foreach (var item in entrada)
+{
+    temp.Add(int.Parse(item));
+}
+
+//Lógica para ordenação
+for (int i = 0; i < entrada.Length; i++)
+{
+    int menor = 0;
+    int j = 0;
+    for (j = 0; j < temp.Count; j++)
+    {
+        if (j == 0)
+        {
+            menor = temp[j];
+        }
+        else if (temp[j] < menor)
+        {
+            menor = temp[j];
+        }
+    }
+    valoresOrdemCrescente.Add(menor);
+    temp.Remove(menor);
+}
+
+//Print no console
+foreach (var v in valoresOrdemCrescente)
+{
+    Console.WriteLine(v);
+}
+Console.WriteLine();
+foreach (var e in entrada)
+{
+    Console.WriteLine(e);
+}
