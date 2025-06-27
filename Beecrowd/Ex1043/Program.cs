636321e84598ba5fe8f3c900363fd22d1f5a04cb
IcaroRaphael <icaro2551@gmail.com>
2025-06-27 08:15:59 -0300

    Ex1043

---
+++ Beecrowd/Ex1043/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] entrada = Console.ReadLine().Split(' ');
+
+double lado1 = double.Parse(entrada[0]);
+double lado2 = double.Parse(entrada[1]);
+double lado3 = double.Parse(entrada[2]);
+
+if (((lado1 + lado2) > lado3) && ((lado1 + lado3) > lado2) && ((lado2 + lado3) > lado1))
+{
+    Console.WriteLine($"Perimetro = {(lado1 + lado2 + lado3):f1}");
+}
+else
+{
+    Console.WriteLine($"Area = {((lado1 + lado2) * lado3 / 2):f1}");
+}
