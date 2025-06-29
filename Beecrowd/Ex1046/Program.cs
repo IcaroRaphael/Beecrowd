7a15fd2e17214c701876cca103467999b6a3db55
IcaroRaphael <icaro2551@gmail.com>
2025-06-29 09:00:43 -0300

    Ex1046

---
+++ Beecrowd/Ex1046/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] entrada = Console.ReadLine().Split(' ');
+
+int inicio = int.Parse(entrada[0]);
+int fim = int.Parse(entrada[1]);
+
+if(fim == inicio)
+{
+    int duracao = 24;
+    Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");
+}
+else if(fim > inicio)
+{
+    int duracao = fim - inicio;
+    Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");
+}
+else if(fim < inicio)
+{
+    int duracao = (24 - inicio) + fim;
+    Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");
+}
